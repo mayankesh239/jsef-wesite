f50f7b14c755ea67199220e3e85cbd752b1db1d7
mayankesh239 <mayankesh.ss@gmail.com>
2022-01-25 17:42:08 +0530

    added poster near video

--- components/faq/index.tsx
+++ components/faq/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import FaqUI from './faq'
 import styles from './faq.module.scss'
-
+import { Image } from '@chakra-ui/react'
 const Faq = () => {
     return (
-        <div>
+        <div className={styles.faq_collection}>
+            <Image className={styles.ifr1}
+                // boxSize='100px'
+                // objectFit='cover'
+                src='https://res.cloudinary.com/mayankesh/image/upload/v1643110669/jsef/JSEF-2-min_ihjbca.jpg'
+                alt='Dan Abramov'
+            />
             {/* <FaqUI /> */}
-            <iframe className={styles.faq_collection} 
+            <iframe className={styles.ifr}
                 src="https://www.youtube.com/embed/73sDcOq8Ss4">
             </iframe>
         </div>
